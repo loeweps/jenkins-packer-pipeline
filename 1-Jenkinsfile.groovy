f53a001d121c62da9e991d5bc91fa47d0d7189f6
loeweps@hotmail.com <loeweps@hotmail.com>
2024-03-06 09:56:59 -0500

    Update Packer build

--- 1-Jenkinsfile.groovy
+++ 1-Jenkinsfile.groovy
@@ -6,7 +6,7 @@ pipeline {
       steps {
         sh """
         #!/bin/bash
-        cd jenkins-tutorial
+        #cd jenkins-tutorial
         packer init .
         packer build -force .
         """
